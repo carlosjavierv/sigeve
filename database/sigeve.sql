44915c661f47ed5096dd9a52b2ba1e845b550b27
carlosjavierv <carxviteri2019@gmail.com>
2020-08-02 12:50:17 -0500

    actualizar db

--- database/sigeve.sql
+++ database/sigeve.sql
@@ -1,127 +1,229 @@
-/*
-Navicat MySQL Data Transfer
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 02-08-2020 a las 12:49:49
+-- Versión del servidor: 10.1.37-MariaDB
+-- Versión de PHP: 7.2.12
 
-Source Server         : Mysql
-Source Server Version : 50505
-Source Host           : localhost:3306
-Source Database       : sigeve
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-Target Server Type    : MYSQL
-Target Server Version : 50505
-File Encoding         : 65001
 
-Date: 2020-08-02 12:30:43
-*/
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-SET FOREIGN_KEY_CHECKS=0;
+--
+-- Base de datos: `sigeve`
+--
 
--- ----------------------------
--- Table structure for rol
--- ----------------------------
-DROP TABLE IF EXISTS `rol`;
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `rol`
+--
+
 CREATE TABLE `rol` (
   `id` int(11) NOT NULL,
-  `nombre` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `nombre` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- ----------------------------
--- Records of rol
--- ----------------------------
-INSERT INTO `rol` VALUES ('1', 'Administrador');
-INSERT INTO `rol` VALUES ('2', 'Conductor');
-INSERT INTO `rol` VALUES ('3', 'Autoridad');
+--
+-- Volcado de datos para la tabla `rol`
+--
 
--- ----------------------------
--- Table structure for ubicacion
--- ----------------------------
-DROP TABLE IF EXISTS `ubicacion`;
+INSERT INTO `rol` (`id`, `nombre`) VALUES
+(1, 'Administrador'),
+(2, 'Conductor'),
+(3, 'Autoridad');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `ubicacion`
+--
+
 CREATE TABLE `ubicacion` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `vehiculo_id` int(11) DEFAULT NULL,
   `latitud` varchar(255) DEFAULT NULL,
-  `longitud` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `ubicacion_vehiculo` (`vehiculo_id`),
-  CONSTRAINT `ubicacion_vehiculo` FOREIGN KEY (`vehiculo_id`) REFERENCES `vehiculo` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=latin1;
+  `longitud` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- ----------------------------
--- Records of ubicacion
--- ----------------------------
+-- --------------------------------------------------------
 
--- ----------------------------
--- Table structure for unidad
--- ----------------------------
-DROP TABLE IF EXISTS `unidad`;
+--
+-- Estructura de tabla para la tabla `unidad`
+--
+
 CREATE TABLE `unidad` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
+  `id` int(11) NOT NULL,
+  `nombre` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- ----------------------------
--- Records of unidad
--- ----------------------------
-INSERT INTO `unidad` VALUES ('1', 'Facultad de medicina 1');
-INSERT INTO `unidad` VALUES ('2', 'Facultad de Ciencias Exactas');
-INSERT INTO `unidad` VALUES ('3', 'Facultad de Economía');
-INSERT INTO `unidad` VALUES ('4', 'Facultad de ciencias de la computación');
-INSERT INTO `unidad` VALUES ('6', 'Facultad de Auditoría');
-INSERT INTO `unidad` VALUES ('7', 'Facultad de Administración');
-INSERT INTO `unidad` VALUES ('8', 'Escuela de Agricultura3');
-INSERT INTO `unidad` VALUES ('9', 'Escuela de educación física1');
-INSERT INTO `unidad` VALUES ('10', 'Facultad de artes');
-INSERT INTO `unidad` VALUES ('11', 'Centro de educación continua');
+--
+-- Volcado de datos para la tabla `unidad`
+--
 
--- ----------------------------
--- Table structure for usuario
--- ----------------------------
-DROP TABLE IF EXISTS `usuario`;
+INSERT INTO `unidad` (`id`, `nombre`) VALUES
+(1, 'Facultad de medicina 1'),
+(2, 'Facultad de Ciencias Exactas'),
+(3, 'Facultad de Economía'),
+(4, 'Facultad de ciencias de la computación'),
+(6, 'Facultad de Auditoría'),
+(7, 'Facultad de Administración'),
+(8, 'Escuela de Agricultura3'),
+(9, 'Escuela de educación física1'),
+(10, 'Facultad de artes'),
+(11, 'Centro de educación continua');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
 CREATE TABLE `usuario` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `rol_id` int(11) NOT NULL,
   `unidad_id` int(11) DEFAULT NULL,
   `nombre` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `email_unique` (`email`),
-  KEY `usuario_rol` (`rol_id`),
-  KEY `usuario_unidad` (`unidad_id`),
-  CONSTRAINT `usuario_rol` FOREIGN KEY (`rol_id`) REFERENCES `rol` (`id`),
-  CONSTRAINT `usuario_unidad` FOREIGN KEY (`unidad_id`) REFERENCES `unidad` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- ----------------------------
--- Records of usuario
--- ----------------------------
-INSERT INTO `usuario` VALUES ('1', '1', null, 'Carlos Viteri', 'carlos@gmail.com', '$2y$10$fEh.Zgf9JpzZBd7xedu5ceViCoqwTY9FEzONHbeQT5hBf7zegET1G');
-INSERT INTO `usuario` VALUES ('3', '3', '7', 'Camila Perez', 'camila@gmail.com', '$2y$10$RdozngAkQdAYNreBEqt2jueHi8MOf8HhJ6FcMZEdHzu1PRTLvMrNa');
-INSERT INTO `usuario` VALUES ('5', '2', null, 'Karla Cespedes', 'karla@gmail.com', '$2y$10$T4P3lbUIb.yrcjvu8NgwTO/frD4IyR6/kYkU6.W1emEV6lfJDVkQu');
-INSERT INTO `usuario` VALUES ('6', '2', null, 'Paulina Sanchez 1', 'paulina@gmail.com', '$2y$10$qVLhnlzca89hjEuGdFJxoen6AHE3ZvkXo9GWJsSjSYkU5IRAGHcbm');
-INSERT INTO `usuario` VALUES ('7', '3', '1', 'Delia Jaramillo', 'delia@gmail.com', '$2y$10$m7XHagMo6/f3SDxSC0yU.OSGVQQvw7/gcDpH.MWzyuurLrKkvunbe');
+--
+-- Volcado de datos para la tabla `usuario`
+--
 
--- ----------------------------
--- Table structure for vehiculo
--- ----------------------------
-DROP TABLE IF EXISTS `vehiculo`;
+INSERT INTO `usuario` (`id`, `rol_id`, `unidad_id`, `nombre`, `email`, `password`) VALUES
+(1, 1, NULL, 'Carlos Viteri', 'carlos@gmail.com', '$2y$10$fEh.Zgf9JpzZBd7xedu5ceViCoqwTY9FEzONHbeQT5hBf7zegET1G'),
+(3, 3, 7, 'Camila Perez', 'camila@gmail.com', '$2y$10$RdozngAkQdAYNreBEqt2jueHi8MOf8HhJ6FcMZEdHzu1PRTLvMrNa'),
+(5, 2, NULL, 'Karla Cespedes', 'karla@gmail.com', '$2y$10$T4P3lbUIb.yrcjvu8NgwTO/frD4IyR6/kYkU6.W1emEV6lfJDVkQu'),
+(6, 2, NULL, 'Paulina Sanchez 1', 'paulina@gmail.com', '$2y$10$qVLhnlzca89hjEuGdFJxoen6AHE3ZvkXo9GWJsSjSYkU5IRAGHcbm'),
+(7, 3, 1, 'Delia Jaramillo', 'delia@gmail.com', '$2y$10$m7XHagMo6/f3SDxSC0yU.OSGVQQvw7/gcDpH.MWzyuurLrKkvunbe');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `vehiculo`
+--
+
 CREATE TABLE `vehiculo` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `unidad_id` int(11) NOT NULL,
   `usuario_id` int(11) NOT NULL,
   `marca` varchar(255) DEFAULT NULL,
   `modelo` varchar(255) DEFAULT NULL,
   `anio` varchar(4) DEFAULT NULL,
-  `placa` varchar(10) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `unique_usuario` (`usuario_id`),
-  KEY `vehiculo_unidad` (`unidad_id`),
-  CONSTRAINT `vehiculo_unidad` FOREIGN KEY (`unidad_id`) REFERENCES `unidad` (`id`),
-  CONSTRAINT `vehiculo_usuario` FOREIGN KEY (`usuario_id`) REFERENCES `usuario` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+  `placa` varchar(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- ----------------------------
--- Records of vehiculo
--- ----------------------------
-INSERT INTO `vehiculo` VALUES ('4', '8', '6', 'JAC', 'S2', '2020', 'PBA-7634');
+--
+-- Volcado de datos para la tabla `vehiculo`
+--
+
+INSERT INTO `vehiculo` (`id`, `unidad_id`, `usuario_id`, `marca`, `modelo`, `anio`, `placa`) VALUES
+(4, 8, 6, 'JAC', 'S2', '2020', 'PBA-7634');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `rol`
+--
+ALTER TABLE `rol`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `ubicacion`
+--
+ALTER TABLE `ubicacion`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `ubicacion_vehiculo` (`vehiculo_id`);
+
+--
+-- Indices de la tabla `unidad`
+--
+ALTER TABLE `unidad`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email_unique` (`email`),
+  ADD KEY `usuario_rol` (`rol_id`),
+  ADD KEY `usuario_unidad` (`unidad_id`);
+
+--
+-- Indices de la tabla `vehiculo`
+--
+ALTER TABLE `vehiculo`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `unique_usuario` (`usuario_id`),
+  ADD KEY `vehiculo_unidad` (`unidad_id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `ubicacion`
+--
+ALTER TABLE `ubicacion`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+
+--
+-- AUTO_INCREMENT de la tabla `unidad`
+--
+ALTER TABLE `unidad`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT de la tabla `vehiculo`
+--
+ALTER TABLE `vehiculo`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `ubicacion`
+--
+ALTER TABLE `ubicacion`
+  ADD CONSTRAINT `ubicacion_vehiculo` FOREIGN KEY (`vehiculo_id`) REFERENCES `vehiculo` (`id`) ON DELETE CASCADE;
+
+--
+-- Filtros para la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD CONSTRAINT `usuario_rol` FOREIGN KEY (`rol_id`) REFERENCES `rol` (`id`),
+  ADD CONSTRAINT `usuario_unidad` FOREIGN KEY (`unidad_id`) REFERENCES `unidad` (`id`);
+
+--
+-- Filtros para la tabla `vehiculo`
+--
+ALTER TABLE `vehiculo`
+  ADD CONSTRAINT `vehiculo_unidad` FOREIGN KEY (`unidad_id`) REFERENCES `unidad` (`id`),
+  ADD CONSTRAINT `vehiculo_usuario` FOREIGN KEY (`usuario_id`) REFERENCES `usuario` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
